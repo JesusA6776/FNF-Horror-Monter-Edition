f93d97b07d80baf5756b3cb24b3ae835350c90e5
JesusA6776 <aquinojesus42690@gmail.com>
2022-08-06 00:38:30 -0700

    full new update

--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -24,12 +24,12 @@ class OutdatedState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
+			"bruh, it looks like you're running an   \n
+			outdated version of Horror Monster Edition (" + MainMenuState.HorrorMonsterEdition + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			Press ESCAPE to proceed anyway.\n
 			\n
-			Thank you for using the Engine!",
+			Thank you for playing the mod!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
@@ -41,7 +41,7 @@ class OutdatedState extends MusicBeatState
 		if(!leftState) {
 			if (controls.ACCEPT) {
 				leftState = true;
-				CoolUtil.browserLoad("https://github.com/ShadowMario/FNF-PsychEngine/releases");
+				CoolUtil.browserLoad("https://github.com/JesusA6776/FNF-Horror-Monter-Edition/releases");
 			}
 			else if(controls.BACK) {
 				leftState = true;
